03e1993f34a35c3a28bb2cd2c6118690c108030f
Sergio Cordero <sergio.crol@gmail.com>
2019-09-20 05:07:35 +0200

    Updates

---
+++ precache-manifest.26182fb62977dcd8b0059f41857613a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a5edc7ffa89e60d07bb0012593200e",
+    "url": "/hitza/index.html"
+  },
+  {
+    "revision": "5b19e93d426b1d76301e",
+    "url": "/hitza/static/css/main.d1a5a51f.chunk.css"
+  },
+  {
+    "revision": "8e72d287007338ae6ba5",
+    "url": "/hitza/static/js/2.4ed425c4.chunk.js"
+  },
+  {
+    "revision": "5b19e93d426b1d76301e",
+    "url": "/hitza/static/js/main.f9d217c1.chunk.js"
+  },
+  {
+    "revision": "8b625877c97699688264",
+    "url": "/hitza/static/js/runtime~main.34cdbc28.js"
+  },
+  {
+    "revision": "3c703b8cd8403ea6726d4c376cf42c0a",
+    "url": "/hitza/static/media/404.3c703b8c.jpg"
+  },
+  {
+    "revision": "ead0da359c3fc3d66ec12126d7fdec41",
+    "url": "/hitza/static/media/exit.ead0da35.svg"
+  },
+  {
+    "revision": "44778f6451df90ddf86124717658aef2",
+    "url": "/hitza/static/media/gnome.44778f64.png"
+  },
+  {
+    "revision": "ce070b7d8999a633603f17eb16af3f51",
+    "url": "/hitza/static/media/heart.ce070b7d.svg"
+  }
+]);
